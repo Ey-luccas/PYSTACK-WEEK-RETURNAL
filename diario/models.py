9889d502bf7c513c47fe02d73acf6e25102317c1
eyluccas <eyluccas@gmail.com>
2025-01-11 15:05:22 -0300

    Atualizações na área de cadastrar a pessoa e o diario

--- diario/models.py
+++ diario/models.py
@@ -11,7 +11,7 @@ class Diario(models.Model):
   titulo = models.CharField(max_length=100)
   tags = models.TextField()
   texto = models.TextField()
-  pessoas = models.ManyToManyField(Pessoa)
+  pessoas = models.ManyToManyField(Pessoa, null= True, blank=True)
   create_at = models.DateField(auto_now_add=True)
 
   def __str__(self): 
--- diario/views.py
+++ diario/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Pessoa
+
 # Create your views here.
 def home(request): 
     return render(request, 'home.html')
 
 def escrever(request): 
     if request.method == 'GET': 
-        return render(request, 'escrever.html')
+        pessoas = Pessoa.objects.all()
+        return render(request, 'escrever.html', {'pessoas': pessoas})
     elif request.method == 'POST': 
         titulo = request.POST.get('titulo')
         tags = request.POST.getlist('tags')
         pessoas = request.POST.getlist('pessoas')
         texto = request.POST.get('texto')
         return HttpResponse(f'{titulo} - {tags} - {pessoas} - {texto}')
+    
+def cadastrar_pessoa(request): 
+    if request.method == 'GET': 
+        return render(request, 'pessoa.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        pessoa = Pessoa(
+            nome = nome,
+            foto = foto
+        )
+        pessoa.save()
+        return redirect('escrever')
